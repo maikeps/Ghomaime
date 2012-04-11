cf01bca732afe40c1c21e1f195f98b83087e39c8
unknown <maike_p_santos@SNSOO1EL17ET011.(none)>
2012-04-11 17:32:33 -0300

    Adicionado um ataque do megaman

--- src/ghomaime/Fase1.java
+++ src/ghomaime/Fase1.java
@@ -21,6 +21,7 @@ public class Fase1 implements GameStateController {
 
     protected ArrayList<TiroNormal> ataque;
     protected ArrayList<TiroVerde> tiroVerde;
+    protected ArrayList<Tiro3> ataque3;
 
     protected Player1 player1;
     protected Player2 player2;
@@ -28,6 +29,7 @@ public class Fase1 implements GameStateController {
     public void load() {
         this.ataque = new ArrayList<TiroNormal>();
         this.tiroVerde = new ArrayList<TiroVerde>();
+        this.ataque3 = new ArrayList<Tiro3>();
         this.player2 = new Player2();
         this.player1 = new Player1();
 
@@ -41,6 +43,9 @@ public class Fase1 implements GameStateController {
             gameObject.step(timeElapsed);
         }
         for (GameObject gameObject : this.tiroVerde) {
+            gameObject.step(timeElapsed);
+        }
+        for (GameObject gameObject : this.ataque3) {
             gameObject.step(timeElapsed);
         }
 
@@ -57,6 +62,9 @@ public class Fase1 implements GameStateController {
             gameObject.draw(g);
         }
         for (GameObject gameObject : this.tiroVerde) {
+            gameObject.draw(g);
+        }
+        for (GameObject gameObject : this.ataque3) {
             gameObject.draw(g);
         }
         
@@ -76,17 +84,21 @@ public class Fase1 implements GameStateController {
 
         if (teclado.keyDown(Keys.P)) {
             if (this.player2.podeAtacar()) {
-                switch (Util.random(3)) {
+                switch (Util.random(4)) {
                     case 1:
                         this.ataque.add(new TiroNormal(player2.getX(), player2.getY(), player2.getDirecao()));
-                        this.player2.setCooldownAtaque(30);
+                        this.player2.setCooldownAtaque(3);
                         this.player2.setImagemAtirando();
                         break;
                     case 2:
                         this.tiroVerde.add(new TiroVerde(player2.getX(), player2.getY(), player2.getDirecao()));
-                        this.player2.setCooldownAtaque(60);
+                        this.player2.setCooldownAtaque(6);
                         this.player2.setImagemAtirando();
                         break;
+                    case 3:
+                        this.ataque3.add(new Tiro3(player2.getX(), player2.getY(), player2.getDirecao()));
+                        this.player2.setCooldownAtaque(6);
+                        this.player2.setImagemAtirando();
                 }
             }
         }
