ffe4bff7c450d01c7c8a2af50f07fa12e2df7300
Maike <maike-ps@hotmail.com>
2012-04-17 17:27:55 -0300

    tudo + alguma coisa

--- src/ghomaime/Fase1.java
+++ src/ghomaime/Fase1.java
@@ -86,15 +86,18 @@ public class Fase1 implements GameStateController {
             if (this.player2.podeAtacar()) {
                 switch (Util.random(4)) {
                     case 1:
+                    case 2:
                         this.ataque.add(new TiroNormal(player2.getX(), player2.getY(), player2.getDirecao()));
                         this.player2.setCooldownAtaque(30);
                         this.player2.setImagemAtirando();
                         break;
-                    case 2:
+                  /*  case 2:
                         this.tiroVerde.add(new TiroVerde(player2.getX(), player2.getY(), player2.getDirecao()));
                         this.player2.setCooldownAtaque(60);
                         this.player2.setImagemAtirando();
                         break;
+                         
+                         */
                     case 3:
                         this.ataque3.add(new Tiro3(player2.getX(), player2.getY(), player2.getDirecao()));
                         this.player2.setCooldownAtaque(60);
